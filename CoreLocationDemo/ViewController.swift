fa19353454922f0cbe3144f6c48b54e899299b76
Sunny NG <ngsanluk@gmail.com>
2017-02-28 04:02:18 +0800

    Initial commit.

--- CoreLocationDemo/ViewController.swift
+++ CoreLocationDemo/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var latitude: UILabel!
+    @IBOutlet weak var longitude: UILabel!
+    @IBOutlet weak var altitude: UILabel!
+    @IBOutlet weak var distance: UILabel!
+    
+    var locationManager = CLLocationManager()
+    var beginningLocation: CLLocation!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        beginningLocation = nil
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +36,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func resetDistance(_ sender: Any) {
+        beginningLocation = nil
+    }
 
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let theMostRecentLocation = locations[locations.count - 1]
+        latitude.text = String(theMostRecentLocation.coordinate.latitude)
+        longitude.text = String(theMostRecentLocation.coordinate.longitude)
+        altitude.text = String(theMostRecentLocation.altitude)
+        print("horizontal accuracy: \(theMostRecentLocation.horizontalAccuracy)")
+        print("vertial accuracy: \(theMostRecentLocation.verticalAccuracy)")
+        
+        if beginningLocation == nil {
+            beginningLocation = theMostRecentLocation
+        }
+        
+        let distanceUpdate = theMostRecentLocation.distance(from: beginningLocation)
+        self.distance.text = String(format: "%.2f", distanceUpdate)
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        // put your error handling codes here
+        print("Error: \(error.localizedDescription)")
+    }
+    
 }
 
